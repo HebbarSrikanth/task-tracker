459731dd33cbb367f0863fe50f4e3cb214e4fe6a
shebbar93 <s.hebbar93@gmail.com>
2021-10-26 19:51:31 +0530

    Completed till adding and deleting the tasks

---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+    <header>
+        <h3>{{title}}</h3>
+        <Button v-show="homePage"  @btn-click="$emit('toggle-add-task')"  
+           :title="showAddToggle?'Close':'Add Task'" 
+           :color="showAddToggle?'red':'green'"/>
+    </header>
+</template>
+
+<script>
+import Button from './Button.vue'
+export default {
+    name:'Header',
+    props:{
+        title:String,
+        showAddToggle:Boolean
+    },
+    components:{
+        Button
+    },
+    computed:{
+        homePage(){
+            if(this.$route.path==='/'){
+                return true
+            }else{
+                return false
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+</style>
